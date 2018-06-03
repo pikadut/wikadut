0fb2d0705006358825ff0b93c8b42354fe036a09
Haqim <khaqim414@gmail.com>
2018-06-03 23:34:07 +0700

    mengganti menu rkap menjadi menu rkp/rkap

--- db/20180603.sql
+++ db/20180603.sql
@@ -1,9 +1,9 @@
 UPDATE adm_menu SET menu_name = 'PR' WHERE menuid = 163;
 UPDATE adm_menu SET menu_name = 'Pembuatan PR' WHERE menuid = 164;
 UPDATE adm_menu SET menu_name = 'Daftar PR' WHERE menuid = 180;
-UPDATE adm_menu SET menu_name = 'RKAP' WHERE menuid = 159;
-UPDATE adm_menu SET menu_name = 'Pembuatan RKAP' WHERE menuid = 160;
-UPDATE adm_menu SET menu_name = 'Data RKAP' WHERE menuid = 161;
-UPDATE adm_menu SET menu_name = 'Review RKAP' WHERE menuid = 162;
-UPDATE adm_menu SET menu_name = 'Update RKAP' WHERE menuid = 196;
-UPDATE adm_menu SET menu_name = 'Rekapitulasi RKAP' WHERE menuid = 199;
+UPDATE adm_menu SET menu_name = 'RKP/RKAP' WHERE menuid = 159;
+UPDATE adm_menu SET menu_name = 'Pembuatan RKP/RKAP' WHERE menuid = 160;
+UPDATE adm_menu SET menu_name = 'Data RKP/RKAP' WHERE menuid = 161;
+UPDATE adm_menu SET menu_name = 'Review RKP/RKAP' WHERE menuid = 162;
+UPDATE adm_menu SET menu_name = 'Update RKP/RKAP' WHERE menuid = 196;
+UPDATE adm_menu SET menu_name = 'Rekapitulasi RKP/RKAP' WHERE menuid = 199;
